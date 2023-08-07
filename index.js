80b68e19239a2f6e1362ff84176920fc936002b3
JosephObote <joeobote01@gmail.com>
2023-08-07 19:40:12 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+let divide = function (){
+    const num1 =2000
+    const num2 = 100
+    return num1/num2
+
+}
+let square = num3 => num3*num3
+
+let add= (num4, num5)=> num4 + num5
+
+let greeting= (name)=> "Goodmorning " + name
+console.log(greeting("Joe"))
